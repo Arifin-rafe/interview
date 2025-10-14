78e3d0bea7c8dedb47754e90feb2c6af9275a6cd
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-10-14 23:55:54 +0600

    interview 59 more

--- frontend59.js
+++ frontend59.js
@@ -11,7 +11,18 @@
 // }
 // console.log(b)
 
+
 // slice vs splice?
 // In JavaScript, slice() returns a shallow copy of a portion of an array without modifying the original.
 // In contrast, splice() changes the original array by adding, removing, or replacing elements.
 // Use slice() for copying and splice() for editing the array.
+
+// what is reduce method?
+// reduce() is a tool in JavaScript that takes all items in an array and turns them into one single value.
+// You tell it how to combine the items, like adding numbers or joining words.
+// It goes through the array one by one and keeps updating the result.
+// In the end, it gives you that final combined value.
+
+//what is normal vs dynamic routing ?
+// Static routing → fixed paths, known ahead of time.
+// 🔄 Dynamic routing → flexible paths that change based on parameters or data.
