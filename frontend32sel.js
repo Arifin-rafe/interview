8eab5791ccce9daf644fdbf70c1f0ba9eccd9af5
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-07 19:08:03 +0600

    added front end 32 sel

---
+++ frontend32sel.js
@@ -0,0 +1,34 @@
+// const str = "abc";
+// const str1 = "123456";
+
+// let result = "";
+
+// for (let i = 0; i < str.length; i++) {
+//   result += str[i];       // add character from str
+//   if (i < str1.length) {  // if str1 still has digits
+//     result += str1[i];    // add corresponding digit from str1
+//   }
+// }
+// // Add remaining characters from str1 (if any)
+// if (str1.length > str.length) {
+//   result += str1.slice(str.length);
+// }
+
+// console.log(result); // a1b2b3456
+
+// Props vs state
+// Props are used to pass data from a parent component to a child component.
+// State is used to manage the internal state of a component.
+
+// life cycle method of functional component?
+// constructor
+// componentDidMount
+// componentDidUpdate
+// componentWillUnmount
+
+// es5 vs es6
+// es5: older version of javascript
+// es6: newer version of javascript with new features and syntax
+//like let/const, arrow functions, classes, destructuring, promises, modules, and more
+
+// difference between functional component and class component
