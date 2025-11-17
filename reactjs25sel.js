f1088f5505773feff5d4da75a0b6c52c2033dcad
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-18 01:41:38 +0600

    react 27sel done

--- reactjs25sel.js
+++ reactjs25sel.js
@@ -1,2 +1,25 @@
-abc = ["a","b","c","d","a","b"]
-console.log(...new Set(abc))
+// var abc = ["a","b","c","d","a","b"]
+// console.log(...new Set(abc))
+
+// HOW web browsers read JSX files?
+//Tools like Babel, Vite, Webpack, Parcel, or Next.js compiler turn
+// JSX into JavaScript before the code ever reaches the browser.
+
+// what is the role of transpiler in react?
+// A transpiler in React is a tool that converts modern JavaScript (ES6+)
+// and JSX syntax into a version of JavaScript that is compatible with older browsers.
+
+// what is hooks?
+// A hook is a special function that allows you to use state and other React features without writing a class component.
+
+//use of html tag?
+//The <html> tag is the root element of an HTML document, 
+//and it contains all the other elements in the document.
+
+// CSS box model?
+// The CSS box model is a concept in CSS that defines how elements are rendered on the screen. 
+// It consists of four main parts: content, padding, border, and margin.
+
+// useContext?
+// The useContext hook is a React hook that allows you to access the value of
+// a React context from a functional component.
