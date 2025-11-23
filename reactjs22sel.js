572ae8722fbf70c2890332c2df1357077d809c2a
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-23 21:26:07 +0600

    react 22 sel for interview

---
+++ reactjs22sel.js
@@ -0,0 +1,46 @@
+//How many types of loops available in js?
+//There are mainly 4 types of loops available in JavaScript:
+//1. for loop
+//2. while loop
+//3. do-while loop
+//4. forEach loop
+
+// defference between for of and for in loop?
+//The main difference between "for...in" and "for...of" loops in JavaScript is in what they iterate over:
+//1. "for...in" loop iterates over the enumerable properties (keys) of an object. It is typically used for objects.
+//2. "for...of" loop iterates over the values of an iterable object, such as an array or a string. 
+//It is typically used for arrays and strings.
+
+// What i a call stack?
+//A call stack is a data structure used by programming languages like JavaScript to keep track of function calls. 
+//It operates on a Last In, First Out (LIFO) principle, meaning the last function that was called is the first one to be completed and removed from the stack. 
+//When a function is invoked, it is added to the top of the call stack, and when it finishes executing, it is removed from the stack. 
+//This helps manage the execution context and ensures that functions are executed in the correct order.
+
+//life cycle method of functional component?
+// constructor
+// componentDidMount
+// componentDidUpdate
+// componentWillUnmount
+
+// what is pure component?
+// Pure components are React components that are optimized for performance by React. 
+// They are pure functions that return JSX without any internal state management.
+
+// what is iterator?
+// An iterator is an object that allows you to iterate over a collection of elements. 
+// It provides a way to access each element in the collection one at a time.
+
+// what is event bubbling?
+// The event first runs any event listeners on the button itself, then it moves up to the parent div,
+// and continues to bubble up to other ancestors until it reaches the root of the document.
+
+// what is use of useRef?
+// We create a reference to the input element using useRef.
+// When the button is clicked, it focuses the input without causing a re-render of the component.
+
+//for design which library you use?
+// Material-UI
+// Bootstrap
+// Tailwind CSS
+// Ant Design
