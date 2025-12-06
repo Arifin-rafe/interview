d8b7bbf18a151e3fbc012dd17783f7ef0c122b8b
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-12-06 12:29:12 +0600

    reactjs 18 sel front end interview

---
+++ reactjs18sel.js
@@ -0,0 +1,25 @@
+// types of web storage?
+// local storage //used for storing data with no expiration date
+// session storage // used for storing data for the duration of the page session
+// cookies
+
+// what are  cookies?
+// small pieces of data stored on the client side by the web browser
+// used to remember information about the user, such as login status, preferences, and tracking data
+
+// what is callback function?
+// A callback function is a function passed into another function as an argument
+
+// what is promise?
+// A Promise is an object representing the eventual completion or failure of an asynchronous operation.
+// It allows you to attach callbacks for success (.then) and failure (.catch) of the async task.
+
+//state object vs peops object?
+// State is a built-in React object used to manage data that can change over time within a component.
+// Props (short for properties) are read-only attributes passed from a parent 
+// component to a child component to configure it.
+
+// what is react strict mode?
+// React Strict Mode is a tool for highlighting potential problems in an application.
+// It activates additional checks and warnings for its descendants, 
+// helping developers identify unsafe lifecycles, deprecated APIs, and other issues during development.
