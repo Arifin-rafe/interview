667df63e5acd1abc108391e4b8a2e9bc531851d9
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-01-19 21:55:06 +0600

    add fjs5.js with JavaScript examples and explanations

---
+++ fjs5.js
@@ -0,0 +1,44 @@
+// let abc = "rafe";
+// abc[0] = "R";
+// console.log(abc); // rafe
+// // strings are immutable in JavaScript
+
+// let a = 100;
+// let b = a++;
+// console.log(a + b); // 201
+
+// const str = "my name is rafe";
+
+// const result = str
+//   .split(" ")
+//   .map((word) => word.split("").reverse().join(""))
+//   .join(" ");
+
+// console.log(result);
+
+// let name = "minhaz rafe";
+// let newName = name.split(" ")[0]
+// console.log(newName);
+
+// let a = 2
+// let b =3 
+// a = b
+// console.log(a,b)
+
+// let a = [1, 2, 3];
+// let b = [2,3,4];
+// let newArr = []
+// for(let i=0; i<a.length; i++){
+//     if(b.includes(a[i])){
+//         newArr.push(a[i])
+//     }
+// }
+// console.log(newArr);
+
+// const arr = [1, 2, 3, 4, 5];
+// const result = arr.reduce((acc, curr) => acc * curr, 1);
+// console.log(result);
+
+// what is polyfill in javascript?
+// Polyfill is a piece of code (usually JavaScript on the Web) used to provide 
+// modern functionality on older browsers that do not natively support it.
