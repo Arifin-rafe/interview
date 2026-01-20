d2b96b4b05090fa74dc7323edf4d13c709188774
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-01-20 22:31:14 +0600

    add fjs4.js for interview preparation

---
+++ fjs4.js
@@ -0,0 +1,42 @@
+// 
+
+// function abc(a,...b,c){ //A rest parameter must be last in a parameter list.
+//     return[...a,...b,c]
+// }
+// console.log(abc([1,2],3,4))
+
+// const name = "minhaz rafe";
+// console.log(name()) // TypeError: name is not a function
+
+// function red(){
+//     return 100+200
+// }
+// var red
+// console.log(red) //return function name
+
+// what is the type of class in javascript?
+// In JavaScript, a class is a special type of function. 
+// The typeof operator returns "function" when applied to a class.
+
+// what is optional chaining in javascript?
+// Optional chaining is a feature in JavaScript that allows you to safely access 
+// deeply nested properties of an object without having to check if each reference 
+// in the chain is valid. It uses the ?. operator to short-circuit and return undefined 
+// if any part of the chain is null or undefined, preventing runtime errors.
+
+// const newName = [..."minhaz rafe"]
+// console.log(newName);
+// Output: ['m', 'i', 'n', 'h', 'a', 'z', ' ', 'r', 'a', 'f', 'e']
+
+// what a parseint function do in javascript?
+// The parseInt function in JavaScript is used to convert a string into an integer. 
+
+// console.log(parseInt("A100")) // NaN 
+// console.log(parseInt("100A")) // 100 
+// console.log(parseInt("10A0")) // 10
+
+// console.log([100,200,300].map(element=>{
+//     if(element>90) return
+//     return element * 10
+// })) // output [undefined, undefined, undefined]
+
