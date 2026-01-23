da59dbd382a92c99acfb032bd98d9b771dcd9c32
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-01-23 23:52:23 +0600

    js practice for interview frontend66

---
+++ frontend66.js
@@ -0,0 +1,27 @@
+// const arr = [1, 2, 3, 4, 5, 6];
+// function maxProduct(arr) {
+//     const sortArr = arr.sort();
+//     console.log(sortArr)
+//     return sortArr[sortArr.length - 2] * sortArr[sortArr.length - 1];
+    
+// }
+// console.log(maxProduct(arr)); // Output: 30
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// arr.length = 0;
+// console.log(arr); // Output: []
+
+// const obj1 = { name: "John", age: 30};
+// const obj2 = { name: "Jane", age: 4 };
+// const merhedobj = { ...obj1, ...obj2 };
+// console.log(merhedobj); // Output: { name: 'Jane', age: 4 }
+
+// what is rest parameters in javascript?
+// Rest parameters allow you to collect multiple arguments into an array or object.
+// Example:
+// function sum(...numbers) {
+//     return numbers.reduce((acc, curr) => acc + curr, 0);
+// }
+// console.log(sum(1, 2, 3, 4, 5)); // Output: 15
+
+
